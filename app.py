38e0a1181480c00e39d9f9cb78d1bd740f3a4b61
bradyoo12 <yoohoony@hotmail.com>
2024-02-19 00:46:21 +0900

    f

---
+++ app.py
@@ -0,0 +1,10 @@
+# https://www.youtube.com/watch?v=dfeM91JG3oQ
+from flask import Flask, jsonify
+app=Flask(__name__)
+
+@app.route("/")
+def root():
+    return jsonify({"Message":"Hello from Flask!!"})
+
+if __name__=="__main__":
+    app.run(debug=True,host="0.0.0.0",port=8080)
